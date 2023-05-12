858f21eacaa3a613099eb07b5924df833e049d95
Jean-Philippe Cote <jean-philippe_cote@hotmail.com>
2023-05-12 12:37:21 -0400

    commencement partie 2

---
+++ chasse/chasse.js
@@ -0,0 +1,18 @@
+// Déclaration de l'objet
+const objChasse = {
+  // Attributs
+  arrIdsPersonnagesAPiger: ["e0001", "e0008", "e0015", "e0019"],
+  arrIdsObjetsAPiger: ["e0002", "e0004", "e0007", "e0021"],
+  arrIdsLieuxAPiger: ["e0005", "e0012", "e0016", "e0022"],
+  // À compléter
+
+  // Méthodes
+  demarrerChasse: function () {
+    // À compléter
+  },
+
+  // À compléter
+};
+
+// Déclaration des écouteurs d'événements
+// À compléter
